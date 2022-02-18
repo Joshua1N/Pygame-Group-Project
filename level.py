11c3c73a08e41ef83c7571d233cf08be2062bf14
Joshua1N <58127593+Joshua1N@users.noreply.github.com>
2022-02-18 10:29:16 -0500

    Add files via upload

---
+++ level.py
@@ -0,0 +1,41 @@
+import pygame
+from tiles import Tile
+from player import Player
+# from chest import Chest
+from rooms.map_one import tile_size
+
+
+class Level:
+    def __init__(self, level_data, surface):
+        # Level Setup
+        self.display_surface = surface
+        self.setup_level(level_data)
+        self.world_shift = 0
+
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+        # self.chests = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+                if cell == 'X':
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player = Player((x, y))
+                    self.player.add(player)
+                '''if cell == 'C':
+                    chest = Chest((x, y), tile_size)
+                    self.chests.add(chest)'''
+
+    def run(self):
+        # Level Tiles
+        self.tiles.update(self.world_shift)
+        self.tiles.draw(self.display_surface)
+
+        # Player
+        self.player.update()
+        self.player.draw(self.display_surface)
+
---
+++ rooms/map_one.py
@@ -0,0 +1,17 @@
+level_map = [
+    '                         ',
+    '                         ',
+    '                         ',
+    'XX      XXX       XX     ',
+    'XX                       ',
+    'XXXX       C XX       XX ',
+    'XXXX   P   XX            ',
+    'XX     X XXXX   XX  XX   ',
+    '       X XXXX   XX  XXX  ',
+    '    XXXX XXXXXX XX  XXXX ',
+    'XXXXXXXX XXXXXX XX  XXXX '
+]
+tile_size = 64
+screen_width = 1920
+screen_height = len(level_map) * tile_size
+
