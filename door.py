01bed58de7acbd1095727ecabd8c7794f0d5d833
Joshua1N <58127593+Joshua1N@users.noreply.github.com>
2022-02-25 10:26:39 -0500

    Delete door.py

--- door.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Door(pygame.sprite.Sprite):
-    def __init__(self, pos, group):
-        super().__init__(group)
-        self.image = pygame.image.load('assets/Door.png')
-        self.image = pygame.transform.scale(self.image, (100, 50))
-        self.rect = self.image.get_rect(topleft=pos)
